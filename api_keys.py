ff21706a5c3f5698cf9cedf84ee8209944c70817
saracasss <spicysara@Saras-Air.domain>
2023-10-30 15:45:00 -0700

    added api_keys.py to .gitignore

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "ac817bc243c9766a0bbf0bd4a57a9ca9"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "df44abe115c94f8a95c9f249b3f20e43"
